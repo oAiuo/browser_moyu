bc49eb7c243b4eda5e018873b374b0204e96b93d
ChenRuitian <1771868300@qq.com>
2021-07-17 17:48:46 +0800

    增加volatle关键词

--- app/src/main/java/com/moyu/browser_moyu/db/MyDataBase.java
+++ app/src/main/java/com/moyu/browser_moyu/db/MyDataBase.java
@@ -16,9 +16,9 @@ import com.moyu.browser_moyu.db.entity.HistoryRecord;
 @Database(entities = {HistoryRecord.class, BookmarkRecord.class}, version = 1)
 @TypeConverters({Converters.class})
 public abstract class MyDataBase extends RoomDatabase {
-    private static final String DATABASE_NAME = "my_db";
+    private static final String DATABASE_NAME = "myDataBase";
 
-    private static MyDataBase databaseInstance =  null;
+    private static volatile MyDataBase databaseInstance =  null;
 
     public static  MyDataBase getInstance(Context context)
     {
