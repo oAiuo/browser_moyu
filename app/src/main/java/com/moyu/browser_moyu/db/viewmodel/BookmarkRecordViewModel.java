f4696421da86b2fccde9a0a25c6eb53d46428cef
ChenRuitian <1771868300@qq.com>
2021-07-17 17:47:43 +0800

    增加viewModel，提供相关操作接口

---
+++ app/src/main/java/com/moyu/browser_moyu/db/viewmodel/BookmarkRecordViewModel.java
@@ -0,0 +1,55 @@
+package com.moyu.browser_moyu.db.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.moyu.browser_moyu.db.MyDataBase;
+import com.moyu.browser_moyu.db.entity.BookmarkRecord;
+import com.moyu.browser_moyu.db.entity.HistoryRecord;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Date;
+import java.util.List;
+
+import io.reactivex.Completable;
+
+public class BookmarkRecordViewModel extends AndroidViewModel {
+
+    //数据库实列
+    private  MyDataBase myDatabase;
+
+
+    private LiveData<List<BookmarkRecord>> bookmarkRecordLiveData;
+
+    public BookmarkRecordViewModel(@NonNull @NotNull Application application) {
+        super(application);
+        myDatabase = MyDataBase.getInstance(application);
+        bookmarkRecordLiveData = myDatabase.bookmarkRecordDao().getBookmarkRecords();
+
+    }
+
+    //新增记录操作
+    public Completable insertBookmarkRecord(final String title, final  String url){
+        BookmarkRecord bookmarkRecord = new BookmarkRecord(title, url);
+        return myDatabase.bookmarkRecordDao().insertBookmarkRecord(bookmarkRecord);
+    }
+
+    //删除单个记录
+    public Completable deleteBookmarkRecord(BookmarkRecord bookmarkRecord){
+        return myDatabase.bookmarkRecordDao().deleteBookmarkRecord(bookmarkRecord);
+    }
+
+    //删除所有记录
+    public Completable deleteBookmarkRecords(){
+        return myDatabase.bookmarkRecordDao().deleteBookMarkRecords();
+    }
+
+    public LiveData<List<BookmarkRecord>> getLiveDataHistoryRecord()
+    {
+        return bookmarkRecordLiveData;
+    }
+}
