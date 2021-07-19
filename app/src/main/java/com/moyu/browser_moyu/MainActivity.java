9dd47dc2f63a9349651fc77cc5e0c0ae6e2e1859
zzc <825815635@qq.com>
2021-07-19 21:00:47 +0800

    navigation list

--- app/src/main/java/com/moyu/browser_moyu/MainActivity.java
+++ app/src/main/java/com/moyu/browser_moyu/MainActivity.java
@@ -16,11 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.fragment_search_page, SearchPageFragment.class, null)
-                    .commit();
-        }
+//        if(savedInstanceState == null){
+//            getSupportFragmentManager().beginTransaction()
+//                    .setReorderingAllowed(true)
+//                    .add(R.id.fragment_search_page, SearchPageFragment.class, null)
+//                    .commit();
+//        }
     }
+
 }
