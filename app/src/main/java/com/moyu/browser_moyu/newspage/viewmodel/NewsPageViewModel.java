eef74f74755a02ae1d651a58fb26e1f64b44e4ec
lulixian <791727361@qq.com>
2021-07-21 23:33:12 +0800

    资讯模块完善

--- app/src/main/java/com/moyu/browser_moyu/newspage/viewmodel/NewsPageViewModel.java
+++ app/src/main/java/com/moyu/browser_moyu/newspage/viewmodel/NewsPageViewModel.java
@@ -7,21 +7,28 @@ import com.moyu.browser_moyu.newspage.entity.Link;
 import com.moyu.browser_moyu.newspage.util.APIutil;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class NewsPageViewModel extends ViewModel {
 
     private MutableLiveData<String> keyWord;
+    private MutableLiveData<List<Link>> urlAndTitle;
 
     public MutableLiveData<String> getKeyWord() {
-        if (keyWord == null) {
-            keyWord = new MutableLiveData<>();
-        }
         return keyWord;
     }
 
-    public NewsPageViewModel() {
+    public MutableLiveData<List<Link>> getUrlAndTitle() {
+        if (urlAndTitle == null) {
+            urlAndTitle = new MutableLiveData<>();
+        }
+        return urlAndTitle;
+    }
 
+    public NewsPageViewModel() {
+        keyWord = new MutableLiveData<>();
+        urlAndTitle = new MutableLiveData<>();
     }
 }
 
